4f4ae0388f49975382fcac9a98a6bc6d02689688
Gavin Grant <gavingrant@gmail.com>
2023-03-31 15:45:06 -0700

    fix: round progress number

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,10 +1,13 @@
+import { textAlign } from "@mui/system";
 import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
   const { progress } = useProgress();
+  const progressToShow = Math.floor(progress);
+
   return (
-    <Html as="div" center style={{ width: "200px" }}>
-      {progress}% complete
+    <Html as="div" center style={{ width: "200px", textAlign: "center" }}>
+      {progressToShow}% complete
     </Html>
   );
 };
